72d04e0589fa912be5b21a1d6012014d15f86f91
amami-harhid <amami-thida100@outlook.jp>
2025-04-26 18:10:43 +0900

    update

--- build/index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'packagetest/build';
---
+++ src/scripts/constant.ts
@@ -0,0 +1,5 @@
+const Jurassic01:string = "Jurassic01";
+const Chill:string = "Chill";
+const AssetHost = "https://amami-harhid.github.io/scratch3likejslib/web";
+
+export {Jurassic01, Chill, AssetHost};
